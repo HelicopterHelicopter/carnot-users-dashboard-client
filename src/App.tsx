1596ed61b2e6d2eba4511d4b9b550265480f07fc
HelicopterHelicopter <jheel787@gmail.com>
2024-05-09 01:05:27 +0530

    added html and css for signup page and some assets

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,17 @@ import Home from './pages/Home'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Profile from './pages/Profile'
+import Users from './pages/Users'
 
 function App() {
 
   return (
     <Routes>
       <Route path='/' element={<Home/>}/>
-      <Route path='/sign-in' element={<SignIn/>}/>
+      <Route path='/login' element={<SignIn/>}/>
       <Route path='/sign-up' element={<SignUp/>}/>
       <Route path='/profile' element={<Profile/>}/>
+      <Route path='/users' element={<Users/>}/>
     </Routes>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import Header from './components/Header.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+      <Header/>
+      <App />
     </BrowserRouter>
-    
+
   </React.StrictMode>,
 )
